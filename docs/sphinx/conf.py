a262c7c7d2f2c4fb175138f2c5c32717ceb5ec3c
LorenzoPierfederici98 <l.pierfederici@studenti.unipi.it>
2025-01-19 16:49:28 +0100

    added doxygen index on sphinx

--- docs/sphinx/conf.py
+++ docs/sphinx/conf.py
@@ -32,4 +32,4 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 html_theme = 'alabaster'
 html_static_path = ['_static']
-html_extra_path = ['../build/html']
+html_extra_path = ['../build/html/index.html']
